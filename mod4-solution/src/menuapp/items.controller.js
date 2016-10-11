c7e1f3bd055045ee3f2e42fa739dd6eec4b3be9b
votuankiet <votuankiet162@gmail.com>
2016-10-11 23:48:32 +0800

    remove console log

--- mod4-solution/src/menuapp/items.controller.js
+++ mod4-solution/src/menuapp/items.controller.js
@@ -8,7 +8,6 @@ angular.module('MenuApp')
 ItemsController.$inject = ['data'];//inject from route state resolve
 function ItemsController(data) {
   var itemsController = this;
-  console.log(data);;
   itemsController.menu_items = data.menu_items;
   itemsController.requestedCategory = data.category.name;
 }
